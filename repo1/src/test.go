651104d09a2c5f71e57a11dc19219e6e679158ac
chaminjae <beingeskimo@gmail.com>
2021-01-11 13:41:36 +0900

    learning

--- repo1/src/test.go
+++ repo1/src/test.go
@@ -1,25 +1,21 @@
-// 패키지 지정
 package main
 
-import "fmt"
-
-// 메인 함수
-func main() {
-	chk(2)
+func nextValue() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
 }
 
-func chk(val int) {
-	switch val {
-	case 1:
-		fmt.Println("1 이하")
-		fallthrough
-	case 2:
-		fmt.Println("2 이하")
-		fallthrough
-	case 3:
-		fmt.Println("3 이하")
-		fallthrough
-	default:
-		fmt.Println("default")
-	}
+func main() {
+	next := nextValue()
+
+	println(next())
+	println(next())
+	println(next())
+
+	anotherNext := nextValue()
+	println(anotherNext())
+	println(anotherNext())
 }
